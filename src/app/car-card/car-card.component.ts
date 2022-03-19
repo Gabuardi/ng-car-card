3c3ea39df1858b42022c9e5b7183da76bc4c6a50
Josué Gabuardi <josuegabuardi@gmail.com>
2022-03-19 00:11:01 -0600

    Finish component

--- src/app/car-card/car-card.component.ts
+++ src/app/car-card/car-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'car-card',
   templateUrl: './car-card.component.html',
   styleUrls: ['./car-card.component.scss']
 })
-export class CarCardComponent implements OnInit {
+export class CarCardComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  @Input() picture: string = ''
+  @Input() brand: string = ''
+  @Input() model: string = ''
+  @Input() year: string = ''
+  @Input() price: string = ''
 
 }
